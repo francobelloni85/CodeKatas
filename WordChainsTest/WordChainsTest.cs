8bf11132b2a071b8371d65a551f297700e3104b8
Francesco Belloni <francobelloni85@libero.it>
2022-04-10 10:42:55 +0200

    [add] WordChainsTest project

---
+++ WordChainsTest/WordChainsTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordChainsTest
+{
+    [TestClass]
+    public class WordChainsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
